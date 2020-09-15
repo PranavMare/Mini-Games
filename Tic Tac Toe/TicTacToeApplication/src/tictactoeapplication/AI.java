d60ed6ae40e1226cc5a03cdb355425831c1e55e5
Pranav <pranavmare99@gmail.com>
2020-09-15 22:26:42 +0530

    Ready to Play

---
+++ Tic Tac Toe/TicTacToeApplication/src/tictactoeapplication/AI.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package tictactoeapplication;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @author PranavMare
+ *
+ * Created on:15-Sep-2020 10:17:28 pm
+ *
+ */
+public class AI {
+
+	public int pickSpot(TicTacToe game) {
+		ArrayList<Integer> choices = new ArrayList<>();
+		for(int i=0;i<9;i++) {
+			if(game.board[i]!='-') {
+				choices.add(i-1);
+			}
+		}
+		Random rand = new Random();
+		int choice =choices.get(Math.abs(rand.nextInt()%choices.size()));
+		return choice;
+	}
+	
+}
